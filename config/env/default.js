37cea7b3a87b40aeb14c7be8b328207d8abadbea
Jinkaz <mokubahg@gmail.com>
2016-08-15 16:44:16 +0900

    Config

--- config/env/default.js
+++ config/env/default.js
@@ -35,6 +35,12 @@ module.exports = {
       limits: {
         fileSize: 1 * 1024 * 1024 // Max file size in bytes (1 MB)
       }
+    },
+    companyPictureUpload: {
+      dest: './modules/companies/client/img/uploads/', // Profile upload destination path
+      limits: {
+        fileSize: 1 * 1024 * 1024 // Max file size in bytes (1 MB)
+      }
     }
   }
 };
